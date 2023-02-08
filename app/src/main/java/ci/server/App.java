96ff25368d3f1f630483a160ecba7438229f1e36
yagiz <skeletrox1@hotmail.com>
2023-02-08 20:05:45 +0100

    Removed commented out code

--- app/src/main/java/ci/server/App.java
+++ app/src/main/java/ci/server/App.java
@@ -11,27 +11,6 @@ public class App {
         } catch (Exception e) {
         e.printStackTrace();
         }
-    /*try{
-        String s;
-        Process p;
-        try {
-            // The -B flag creates the branch if it doesn't already exist, and resets it if it exists
-            p = Runtime.getRuntime().exec("git checkout -B assessment");
-            BufferedReader brOut = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            BufferedReader brError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-            while ((s = brOut.readLine()) != null)
-                System.out.println("line: " + s);
-
-            while ((s = brError.readLine()) != null)
-                System.out.println("Eline: " + s);
-
-            p.waitFor();
-            System.out.println ("exit: " + p.exitValue());
-            p.destroy();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
     }
 
 }
