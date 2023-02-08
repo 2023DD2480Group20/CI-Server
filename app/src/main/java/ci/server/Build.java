05be666d6be7f55cf4ed9d2100382b0afce9d153
Ronan <ronan.tanios@gmail.com>
2023-02-09 00:31:44 +0100

    fix: fix the commands given to the process

--- app/src/main/java/ci/server/Build.java
+++ app/src/main/java/ci/server/Build.java
@@ -11,35 +11,44 @@ import java.util.*;
 public final class Build{
     private final File local_directory;
 
-    public Build(RepositoryBuilder repositoryBuilder){
-        this.local_directory = repositoryBuilder.local_directory;
+    public Build(File local_directory){
+        this.local_directory = local_directory;
     }
 
     //TODO should return the results needed by Notify
     public void build(){
         try {
             // In the continuous server, cd to the directory of the local file and build the project
-            ArrayList<String> command = new ArrayList<>();
-            command.add("cd " + local_directory.getAbsolutePath());
-            command.add("gradle build");
+            String command = "cd " + local_directory.getAbsolutePath();
             ProcessBuilder builder = new ProcessBuilder(command);
             Process process = builder.start();
 
             // wait for the process to finish
             int exitCode = process.waitFor();
-
-            if(exitCode == 0){
-                // Read the output of the command line
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                FileWriter writer = new FileWriter("result.txt");
-                String line;
-
-                // Write the output of the command line to the result.txt
-                while ((line = reader.readLine()) != null){
-                    writer.write(line + System.lineSeparator());
-                }
-                writer.close();
+            if(exitCode != 0){
+                return;
             }
+
+            command = "gradle build";
+            builder = new ProcessBuilder(command);
+            process = builder.start();
+
+            exitCode = process.waitFor();
+            if(exitCode != 0){
+                return;
+            }
+
+            // Read the output of the command line
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            FileWriter writer = new FileWriter("result.txt");
+            String line;
+
+            // Write the output of the command line to the result.txt
+            while ((line = reader.readLine()) != null){
+                writer.write(line + System.lineSeparator());
+            }
+            writer.close();
+
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
