4d58aec7db5083894d029d2f85255e27244c3f5f
MarcusSamuelsson <Samuelsson.marcus17@gmail.com>
2023-02-09 22:52:12 +0100

    fix: forgot to save one file

--- app/src/test/java/ci/server/ContinuousIntegrationServerTest.java
+++ app/src/test/java/ci/server/ContinuousIntegrationServerTest.java
@@ -83,20 +83,4 @@ public class ContinuousIntegrationServerTest {
 
         assertThat(ContinuousIntegrationServer.extractCommitSha(json,"push"), equalTo("22a43014e5d79a491990291a201d0f2afcbf61a5"));
     }
-
-    @Test void notifyTest(){
-        String sha = "79e52d36ead03613d4c4c3e2df1d41e1663cd3a3";
-        String test = "";
-
-
-        try{
-            JSONObject json = new JSONObject(Notify.changeStatus(sha,"pending"));
-            test = json.getString("state");
-            //test = Notify.changeStatus(sha,"pending");
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        
-        assertThat(test, equalTo("pending"));
-    }
 }
