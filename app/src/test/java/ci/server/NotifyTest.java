b9633c5ab068bbc7ee6197297e4b5bf470c8042d
yagiz <skeletrox1@hotmail.com>
2023-02-10 15:48:05 +0100

    Deleting the temporary clone when build finished and removing unused imports

--- app/src/test/java/ci/server/NotifyTest.java
+++ app/src/test/java/ci/server/NotifyTest.java
@@ -1,28 +1,10 @@
 package ci.server;
 
-import DTO.TunnelsDTO;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.checkerframework.checker.units.qual.C;
-import org.eclipse.jetty.util.log.Log;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.matchesPattern;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
-import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.json.*;
